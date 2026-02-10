0a171974f8b089d9254078a965e39d576dcf8470
velcoinvlc <daiema2@yahoo.com>
2026-02-10 12:25:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os print("Wallet fundadora detectada:", os.environ.get("VELCOIN_FUND_WALLET"))
 from flask import Flask, jsonify, request
 import json, os, hashlib, time, threading, logging, random, string
 
